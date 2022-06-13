d8ec25eae2c0ccb9a8aedcd2cd86c167d48224b2
Muniox <muniox20@gmail.com>
2022-06-13 15:04:55 +0200

    Initial files

--- d22_node_http/index.js
+++ d22_node_http/index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 http.createServer(
-    () => {
-        console.log('Hello, World!');
+    (req, res) => {
+        res.writeHead(200, {
+           'Content-type': 'text/html; charset=utf-8'
+
+        });
+        res.end('<h1>Działa</h1>');
     }
 ).listen(3000, '127.0.0.1');
